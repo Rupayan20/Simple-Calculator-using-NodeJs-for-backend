1e4c796fe322f74a58a9f16830f0cd66720b6793
Rupayan20 <102848153+Rupayan20@users.noreply.github.com>
2022-12-20 23:59:51 +0530

    Create main.js

---
+++ server/main.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+app.post("/", function (req, res) {
+  var n1 = Number(req.body.n1);
+  var n2 = Number(req.body.n2);
+  var result = n1 + n2;
+  res.send("The sum of the two numbers you entered is: " + result);
+});
+
+app.listen(8000, function () {
+  console.log("The code is running now on port 8000");
+});
